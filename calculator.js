4e0bc5e192bc0081246145e5b1cd9b942111024d
Kishan Patel <84789603+Kishnptll@users.noreply.github.com>
2021-06-14 12:41:28 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,45 +1,12 @@
 <html>
 <head>
-
 	<title> Calculator </title>
 </head>
+/* JAVASCRIPT */
 
-<script language="javascript">
-	function op(str)
-	{
-		if(!Number(f1.no1.value) || !Number(f1.no2.value))
-		{
-			alert("Not a Number");
-			reset();
-		}
-		else
-		{
-			if(str=="add")
-			{
-				f1.res.value=parseInt(f1.no1.value)+parseInt(f1.no2.value);
-			}
-			if(str=="sub")
-			{
-				f1.res.value=(f1.no1.value)-(f1.no2.value);
-			}
-			if(str=="mul")
-			{
-				f1.res.value=(f1.no1.value)*(f1.no2.value);
-			}
-			if(str=="div")
-			{
-				f1.res.value=(f1.no1.value)/(f1.no2.value);
-			}
-		}
-	}
-	function reset()
-	{
-		f1.no1.value="";
-		f1.no2.value="";
-		f1.no3.value="";
-	}
-	
-	</script>
+
+/* CSS */
+
 	
 	<body>
 	<form name="f1">
