7f1d7f45f4061df4e9ded3f18665cecc2d588e26
Kishan Patel <84789603+Kishnptll@users.noreply.github.com>
2021-06-14 13:36:02 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,36 +1,26 @@
-<script language="javascript">
-	function op(str)
-	{
-		if(!Number(f1.no1.value) || !Number(f1.no2.value))
-		{
-			alert("Not a Number");
-			reset();
-		}
-		else
-		{
-			if(str=="add")
-			{
-				f1.res.value=parseInt(f1.no1.value)+parseInt(f1.no2.value);
-			}
-			if(str=="sub")
-			{
-				f1.res.value=(f1.no1.value)-(f1.no2.value);
-			}
-			if(str=="mul")
-			{
-				f1.res.value=(f1.no1.value)*(f1.no2.value);
-			}
-			if(str=="div")
-			{
-				f1.res.value=(f1.no1.value)/(f1.no2.value);
-			}
-		}
-	}
-	function reset()
-	{
-		f1.no1.value="";
-		f1.no2.value="";
-		f1.no3.value="";
-	}
-	
-	</script>
+<!-- Begins the JavaScript Code -->
+<script>
+
+// Use insert() function to insert the number in textview.
+function insert(num) 
+{
+document.form1.textview.value = document.form1.textview.value + num;
+}
+
+// Use equal() function to return the result based on passed values.
+function equal()
+{
+var exp = document.form1.textview.value;
+if(exp)
+{
+document.form1.textview.value = eval(exp)
+}
+}
+
+/* Here, we create a backspace() function to remove the number at the end of the numeric series in textview. */
+function backspace()
+{
+var exp = document.form1.textview.value;
+document.form1.textview.value = exp.substring(0, exp.length - 1); /* remove the element from total length ? 1 */
+}
+</script>
